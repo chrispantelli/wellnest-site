e3f81bf3945e10764d077a61c0e17c744e6f2f17
Chris Pantelli <chrispantelli@Chriss-MacBook-Pro.local>
2023-09-11 21:54:34 +0100

    Updated

--- sections/Kits.jsx
+++ sections/Kits.jsx
@@ -10,40 +10,32 @@ export default function Kits() {
                     <div>
                         <img src="products/BoxMock_For Him.jpg" className="rounded-2xl" />
                         <h6 className="heading-6 text-dark-blue pt-4">Wellnest for Him</h6>
-                        <p className="paragraph-large-normal text-primary-blue">Health</p>
+                        <p className="paragraph-large-normal text-primary-blue">Popular</p>
                     </div>
                     <div>
                         <img src="products/BoxMock_For Her.jpg" className="rounded-2xl" />
                         <h6 className="heading-6 text-dark-blue pt-4">Wellnest for Her</h6>
-                        <p className="paragraph-large-normal text-primary-blue">Health</p>
+                        <p className="paragraph-large-normal text-primary-blue">Popular</p>
                     </div>
                     <div>
-                        <div className="bg-light-blue h-72 rounded-2xl">
-
-                        </div>
+                        <img src="products/BoxMock_Intolerance & Allergies.jpg" className="rounded-2xl" />
                         <h6 className="heading-6 text-dark-blue pt-4">Premium Intolerance Test</h6>
                         <p className="paragraph-large-normal text-primary-blue">Health</p>
                     </div>
                     <div>
-                        <div className="bg-light-blue h-72 rounded-2xl">
-
-                        </div>
-                        <h6 className="heading-6 text-dark-blue pt-4">Premium Intolerance Test</h6>
-                        <p className="paragraph-large-normal text-primary-blue">Health</p>
+                        <img src="products/BoxMock_Essential Vitamins Test.jpg" className="rounded-2xl" />
+                        <h6 className="heading-6 text-dark-blue pt-4">Essential Health Test</h6>
+                        <p className="paragraph-large-normal text-primary-blue">Full Body Health</p>
                     </div>
                     <div>
-                        <div className="bg-light-blue h-72 rounded-2xl">
-
-                        </div>
-                        <h6 className="heading-6 text-dark-blue pt-4">Premium Intolerance Test</h6>
-                        <p className="paragraph-large-normal text-primary-blue">Health</p>
+                        <img src="products/BoxMock_Male Hormone Test.jpg" className="rounded-2xl" />
+                        <h6 className="heading-6 text-dark-blue pt-4">Male Hormone Test</h6>
+                        <p className="paragraph-large-normal text-primary-blue">Gut Health</p>
                     </div>
                     <div>
-                        <div className="bg-light-blue h-72 rounded-2xl">
-
-                        </div>
-                        <h6 className="heading-6 text-dark-blue pt-4">Premium Intolerance Test</h6>
-                        <p className="paragraph-large-normal text-primary-blue">Health</p>
+                        <img src="products/BoxMock_Female Hormone Test.jpg" className="rounded-2xl" />
+                        <h6 className="heading-6 text-dark-blue pt-4">Female Hormone Test</h6>
+                        <p className="paragraph-large-normal text-primary-blue">Womens</p>
                     </div>
                 </div>
             </div>
