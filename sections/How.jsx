f4f352e1767b5eda138a184b8a4ce6035e080d13
Chris Pantelli <chrispantelli@Chriss-MacBook-Pro.local>
2023-09-11 22:03:21 +0100

    Updated

--- sections/How.jsx
+++ sections/How.jsx
@@ -5,7 +5,7 @@ export default function How() {
                 <h2 className="heading-2 text-white text-center z-50">How Wellnest Works</h2>
                 <div className="grid lg:grid-cols-3 gap-12 pt-20">
                     <div>
-                       <img src="https://store.chronomics.com/cdn/shop/files/howitworks_1_180x.png?v=1653489425" className="mx-auto" />
+                       <img src="howitworks_1_180x.png" className="mx-auto" />
                         <div>
                             <h6 className="heading-6 text-white pt-8 text-center">Step 01</h6>
                             <h4 className="heading-4 text-white text-center">Pick your test</h4>
@@ -13,7 +13,7 @@ export default function How() {
                         </div>
                     </div>
                     <div>
-                    <img src="https://store.chronomics.com/cdn/shop/files/howitworks_2_180x.png?v=1653490750" className="mx-auto" />
+                    <img src="howitworks_2_180x.avif" className="mx-auto" />
                         <div>
                             <h6 className="heading-6 text-white pt-8 text-center">Step 02</h6>
                             <h4 className="heading-4 text-white text-center">Collect your sample</h4>
@@ -21,7 +21,7 @@ export default function How() {
                         </div>
                     </div>
                     <div>
-                    <img src="https://store.chronomics.com/cdn/shop/files/howitworks_3_180x.png?v=1653490750" className="mx-auto" />
+                    <img src="howitworks_3_180x.png" className="mx-auto" />
 
                         <div>
                         <h6 className="heading-6 text-white pt-8 text-center">Step 03</h6>
